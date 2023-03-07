ae2271b000b94f0166f4a660dbc830bf66366b31
Utku Altinay <altinayutku@gmail.com>
2023-03-08 01:18:13 +0300

    groupchatactivate

--- app/src/main/java/com/example/chatapplication/GroupActivity.kt
+++ app/src/main/java/com/example/chatapplication/GroupActivity.kt
@@ -1,28 +1,27 @@
 package com.example.chatapplication
-
-import android.content.Intent
 import android.graphics.drawable.ColorDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat.getColor
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.chatapplication.databinding.ActivityChatBinding
+import com.example.chatapplication.GroupAdapter
+import com.example.chatapplication.Message
+import com.example.chatapplication.R
 import com.example.chatapplication.databinding.ActivityGroupBinding
-import com.example.chatapplication.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
+
+
+
+
 
 class GroupActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGroupBinding
 
-    private lateinit var mDbRef:DatabaseReference
+    private lateinit var mDbRef: DatabaseReference
     private lateinit var messageAdapter:GroupAdapter
     private lateinit var auth: FirebaseAuth
     private lateinit var messageList:ArrayList<Message>
@@ -31,46 +30,46 @@ class GroupActivity : AppCompatActivity() {
         binding = ActivityGroupBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
         val name = intent.getStringExtra("names")
         supportActionBar?.title="Elmalar"
         supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.orange)))
-        auth=Firebase.auth
+        auth= Firebase.auth
         var username = auth.currentUser!!.email.toString()
         var currentusername = username
-        mDbRef=FirebaseDatabase.getInstance().getReference()
+        mDbRef= FirebaseDatabase.getInstance().getReference()
         messageList= ArrayList()
-           messageAdapter = GroupAdapter(this,messageList)
-           binding.chatRecyclerView.adapter=messageAdapter
-           binding.chatRecyclerView.layoutManager= LinearLayoutManager(this)
+        messageAdapter = GroupAdapter(this,messageList)
+        binding.chatRecyclerView.adapter=messageAdapter
+        binding.chatRecyclerView.layoutManager= LinearLayoutManager(this)
+
+
+        mDbRef.child("chats").child("GrupSohbet").child("messages").addValueEventListener(object:
+            ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                messageList.clear()
+                for (postSnapshot in snapshot.children){
+                    val message = postSnapshot.getValue(Message::class.java)
+
+
+                    //  var msg =    message?.username.toString()
 
 
 
-               mDbRef.child("chats").child("GrupSohbet").child("messages").addValueEventListener(object:
-                   ValueEventListener {
-                   override fun onDataChange(snapshot: DataSnapshot) {
-                       messageList.clear()
-                       for (postSnapshot in snapshot.children){
-                           val message = postSnapshot.getValue(Message::class.java)
-
-
-                      //  var msg =    message?.username.toString()
+                    messageList.add(message!!)
 
 
 
-                           messageList.add(message!!)
+                }
+                messageAdapter.notifyDataSetChanged()
 
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@GroupActivity, error.message, Toast.LENGTH_SHORT).show()
+            }
 
-                       }
-                       messageAdapter.notifyDataSetChanged()
-
-                   }
-
-                   override fun onCancelled(error: DatabaseError) {
-                       Toast.makeText(this@GroupActivity, error.message, Toast.LENGTH_SHORT).show()
-                   }
-
-               })
+        })
 
 
 
@@ -86,7 +85,7 @@ class GroupActivity : AppCompatActivity() {
 
             mDbRef.child("chats").child("GrupSohbet").child("messages").push()
                 .setValue(Message(username,message,"s")).addOnSuccessListener {
-                   // Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
+                    // Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
 
 
 
@@ -94,6 +93,8 @@ class GroupActivity : AppCompatActivity() {
 
                 }
         }
+
+
 
 
 
